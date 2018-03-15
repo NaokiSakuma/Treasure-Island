007338039dd2befc07a343d6ccbdf161a8254186
konji <konjikk15@gmail.com>
2018-03-15 22:50:32 +0900

    敵の船の移動(初期イメージ)

---
+++ Assets/Scripts/Koshida/EnemyShip.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UniRx;
+using UniRx.Triggers;
+
+namespace Konji
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class EnemyShip : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform _target;
+
+        private NavMeshAgent _navAgent;
+
+        private void Awake()
+        {
+            _navAgent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Start()
+        {
+            if (_target != null)
+            {
+                _navAgent.destination = _target.position;
+            }
+        }
+    }
+}
+
