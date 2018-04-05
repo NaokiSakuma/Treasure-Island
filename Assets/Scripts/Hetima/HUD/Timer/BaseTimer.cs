bdf240534ead60aadf9fedc3e529ebb87d633f39
ヘチマ <hetima333@gmail.com>
2018-04-05 11:17:40 +0900

    タイマー追加、仮のタイマーで現状は対応

---
+++ Assets/Scripts/Hetima/HUD/Timer/BaseTimer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UniRx;
+
+public class BaseTimer : MonoBehaviour {
+	// 残り時間
+	protected IntReactiveProperty _remainingTime = new IntReactiveProperty(60);
+	public IntReactiveProperty RemainingTime {
+		get { return _remainingTime; }
+	}
+}
---
+++ Assets/Scripts/Hetima/HUD/Timer/TimerText.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class TimerText : MonoBehaviour {
+
+	[SerializeField]
+	public BaseTimer _timer;
+
+	// Use this for initialization
+	void Start () {
+
+		var text = GetComponentInChildren<Text>();
+
+		// テキスト表示の更新
+		_timer.RemainingTime
+		      .Subscribe(x => {
+				text.text = x / 60 + ":" + (x % 60 >= 10 ? "" : "0") + x % 60;
+			});
+	}
+}
