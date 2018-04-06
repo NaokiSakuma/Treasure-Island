250bf98fbda2f7a681065222e90597d82f56a03f
sakuma <soramamenatan@gmail.com>
2018-04-06 16:15:30 +0900

    一時コミット

---
+++ Assets/Scripts/Sakuma/IEventTrigger.cs
@@ -0,0 +1,5 @@
+﻿interface IEventTrigger
+{
+    void StartEvent();
+    void EndEvent();
+}
---
+++ Assets/Scripts/Sakuma/TimerEventTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerEventTrigger : MonoBehaviour,IEventTrigger {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartEvent()
+    {
+
+    }
+
+    public void EndEvent()
+    {
+
+    }
+}
