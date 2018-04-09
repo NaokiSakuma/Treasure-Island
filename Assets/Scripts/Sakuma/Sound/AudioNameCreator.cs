a1f1a8513edfe9a927af461858bb9a0078b56295
sakuma <soramamenatan@gmail.com>
2018-04-10 00:52:20 +0900

    soundManagerとテストScene完成

---
+++ Assets/Scripts/Sakuma/Sound/AudioNameCreator.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Text;
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditorInternal;
+#endif
+using UnityEngine;
+
+/// <summary>
+/// オーディオのファイル名を定数で管理するクラスを作成するスクリプト
+/// </summary>
+public static class AudioNameCreator
+{
+
+    private const string COMMAND_NAME = "Tools/Create/Audio Name";        // コマンド名
+    private const string EXPORT_PATH = "Assets/Scripts/Constants/AUDIO.cs"; //作成したスクリプトを保存するパス
+
+    // ファイル名(拡張子あり、なし)
+    private static readonly string FILENAME = Path.GetFileName(EXPORT_PATH);
+    private static readonly string FILENAME_WITHOUT_EXTENSION = Path.GetFileNameWithoutExtension(EXPORT_PATH);
+
+    /// <summary>
+    /// オーディオのファイル名を定数で管理するクラスを作成します
+    /// </summary>
+    [MenuItem(COMMAND_NAME)]
+    public static void Create()
+    {
+        if (!CanCreate())
+        {
+            return;
+        }
+
+        CreateScript();
+
+        EditorUtility.DisplayDialog(FILENAME, "作成が完了しました", "OK");
+    }
+
+    /// <summary>
+    /// スクリプトを作成します
+    /// </summary>
+    public static void CreateScript()
+    {
+        StringBuilder builder = new StringBuilder();
+
+        builder.AppendLine("/// <summary>");
+        builder.AppendLine("/// オーディオ名を定数で管理するクラス");
+        builder.AppendLine("/// </summary>");
+        builder.AppendFormat("public static class {0}", FILENAME_WITHOUT_EXTENSION).AppendLine();
+        builder.AppendLine("{");
+
+        //指定したパスのリソースを全て取得
+        object[] bgmList = Resources.LoadAll("Audio/BGM");
+        object[] seList = Resources.LoadAll("Audio/SE");
+
+        foreach (AudioClip bgm in bgmList)
+        {
+            builder.Append("\t").AppendFormat(@"  public const string BGM_{0} = ""{1}"";", bgm.name.ToUpper(), bgm.name).AppendLine();
+        }
+
+        builder.AppendLine("\t");
+
+        foreach (AudioClip se in seList)
+        {
+            builder.Append("\t").AppendFormat(@"  public const string SE_{0} = ""{1}"";", se.name.ToUpper(), se.name).AppendLine();
+        }
+
+        builder.AppendLine("}");
+
+        string directoryName = Path.GetDirectoryName(EXPORT_PATH);
+        if (!Directory.Exists(directoryName))
+        {
+            Directory.CreateDirectory(directoryName);
+        }
+
+        File.WriteAllText(EXPORT_PATH, builder.ToString(), Encoding.UTF8);
+        AssetDatabase.Refresh(ImportAssetOptions.ImportRecursive);
+    }
+
+    /// <summary>
+    /// オーディオのファイル名を定数で管理するクラスを作成できるかどうかを取得します
+    /// </summary>
+    [MenuItem(COMMAND_NAME, true)]
+    private static bool CanCreate()
+    {
+        return !EditorApplication.isPlaying && !Application.isPlaying && !EditorApplication.isCompiling;
+    }
+
+}
---
+++ Assets/Scripts/Sakuma/Sound/AudioTest.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using System;
+public class AudioTest : MonoBehaviour {
+    [SerializeField]
+    private bool _nextBgm = false;
+    [SerializeField]
+    private bool _nextSe1 = false;
+    [SerializeField]
+    private bool _nextSe2 = false;
+
+    // Use this for initialization
+    void Start () {
+        AudioManager.Instance.PlayBGM(AUDIO.BGM_PLAYBGM);
+        //this.UpdateAsObservable()
+        //    .First()
+        //    .Where(_ => _nextBgm)
+        //    .Subscribe(_ => AudioManager.Instance.PlayBGM(AUDIO.BGM_TITLEBGM));
+        //this.UpdateAsObservable()
+        //    .Where(_ => _nextSe1)
+        //    .Subscribe(_ => AudioManager.Instance.PlaySE(AUDIO.SE_BATTLE));
+        //this.UpdateAsObservable()
+        //    .Where(_ => _nextSe2)
+        //    .Subscribe(_ => AudioManager.Instance.PlaySE(AUDIO.SE_OCCUPATION));
+        //Observable.Interval(TimeSpan.FromMilliseconds(3000))
+        //    .Subscribe(_ =>
+        //    {
+        //        AudioManager.Instance.PlaySE(AUDIO.SE_SHIP);
+        //        //AudioManager.Instance.PlaySE(AUDIO.SE_BATTLE);
+        //    }
+        //    );
+        //SoundManager.Instance.PlayBgm(AUDIO.BGM_PLAYBGM);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if(_nextBgm)
+        {
+            AudioManager.Instance.PlayBGM(AUDIO.BGM_TITLEBGM,0.5f);
+            _nextBgm = false;
+        }
+        if (_nextSe1)
+        {
+            AudioManager.Instance.PlaySE(AUDIO.SE_SHIP);
+            _nextSe1 = false;
+        }
+        if (_nextSe2)
+        {
+            AudioManager.Instance.PlaySE(AUDIO.SE_SEPARATE);
+            _nextSe2 = false;
+        }
+
+    }
+}
