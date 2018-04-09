e4b491a0527dccfb624e3ecadaecb586614120e0
sakuma <soramamenatan@gmail.com>
2018-04-09 11:36:29 +0900

    titleSceneの実装　scene先の名前が不明のため、今は飛べない

--- Assets/Scripts/Sakuma/Title/TitleScene.cs
+++ Assets/Scripts/Sakuma/Title/TitleScene.cs
@@ -1,16 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine.SceneManagement;
 
 public class TitleScene : MonoBehaviour {
 
+    // ボタン
+    [SerializeField]
+    private Button _button;
+    // フェードのrange用
+    [SerializeField]
+    private FadeImage _fadeImage;
+    // フェードする速度
+    [SerializeField]
+    private float _fadeSpeed = 0f;
 	// Use this for initialization
 	void Start () {
-		
+        this._button.onClick.AsObservable()
+            .First()
+            .Subscribe(_ => StartCoroutine(FadeOut()));
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    /// <summary>
+    /// フェードアウト
+    /// </summary>
+    /// <returns>フェードアウトしきるまで再帰</returns>
+    private IEnumerator FadeOut()
+    {
+        _fadeImage.Range += _fadeSpeed;
+        if (_fadeImage.Range <= 1.0f)
+        {
+            yield return null;
+            yield return StartCoroutine(FadeOut());
+        }
+        // SceneManager.LoadScene("名前");
+        yield break;
+    }
+
 }
