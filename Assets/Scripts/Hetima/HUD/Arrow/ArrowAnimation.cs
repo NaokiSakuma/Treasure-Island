a3f080911166b8ee9c39631cb3f1add92657565c
ヘチマ <hetima333@gmail.com>
2018-04-09 23:15:38 +0900

    矢印のアニメーション追加

--- Assets/Scripts/Hetima/HUD/Arrow/ArrowAnimation.cs
+++ Assets/Scripts/Hetima/HUD/Arrow/ArrowAnimation.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UniRx;
+using UniRx.Triggers;
 
 public class ArrowAnimation : MonoBehaviour {
 
@@ -12,27 +13,106 @@ public class ArrowAnimation : MonoBehaviour {
 	[SerializeField]
 	private Image[] _arrows;
 
+	[SerializeField, Range(0.0f, 10.0f)]
+	public float _scalingSpeed = 1.0f;
+
+	[SerializeField, Range(1.0f, 2.0f)]
+	public float _scaleLimit = 2.0f;
+
 	// Use this for initialization
 	void Start () {
 		var mm = _moveMap;
 
 		_arrows = GetComponentsInChildren<Image>();
 
-		//foreach(var item in _arrows){
-		//	this.ObserveEveryValueChanged(x => mm.MoveDirection)
-		//		.Where(move => move.sqrMagnitude > 0.0f)
-		//	    .Where(_ => item.transform.localScale.x <= 1.5f)
-		//		.Subscribe(move => {
-		//			item.rectTransform.localScale += Vector3.one * Time.deltaTime;
-		//		});	
-		//}
+		// くっそ汚いのでうまいことまとめる方法知ってたらおしえてクレメンス
+		// === 左の矢印 ===
+		// 拡大
+		this.ObserveEveryValueChanged(x => mm.MoveDirection.x)
+		    .Where(x => x < 0.0f)
+		    .Where(x => _arrows[0].transform.localScale.x < _scaleLimit)
+			.Subscribe(_ => {
+				_arrows[0].transform.localScale += Vector3.one * _scalingSpeed * Time.deltaTime;
+				if (_arrows[0].transform.localScale.x > _scaleLimit) {
+					_arrows[0].transform.localScale = Vector3.one * _scaleLimit;
+				}
+			});
 
-		this.ObserveEveryValueChanged(x => mm.MoveDirection)
-				.Where(move => move.x <= 0.0f)
-		   		.Where(_ => _arrows[0].transform.localScale.x >= 1.0f)
-				.Subscribe(move => {
-					Debug.Log(_arrows[0].transform.localScale);
-					_arrows[0].transform.localScale -= Vector3.one * Time.deltaTime;
-				});	
+		// 縮小
+		this.UpdateAsObservable()
+		    .Where(x => mm.MoveDirection.x >= 0.0f)
+			.Where(x => _arrows[0].transform.localScale.x > 1.0f)
+			.Subscribe(_ => {
+				_arrows[0].transform.localScale -= Vector3.one * _scalingSpeed * Time.deltaTime;
+				if (_arrows[0].transform.localScale.x < 1.0f) {
+					_arrows[0].transform.localScale = Vector3.one;
+				}
+			});
+
+		// === 右の矢印 ===
+		// 拡大
+		this.ObserveEveryValueChanged(x => mm.MoveDirection.x)
+			.Where(x => x > 0.0f)
+			.Where(x => _arrows[1].transform.localScale.x < _scaleLimit)
+			.Subscribe(_ => {
+				_arrows[1].transform.localScale += Vector3.one * _scalingSpeed * Time.deltaTime;
+				if (_arrows[1].transform.localScale.x > _scaleLimit) {
+					_arrows[1].transform.localScale = Vector3.one * _scaleLimit;
+				}
+			});
+		// 縮小
+		this.UpdateAsObservable()
+			.Where(x => mm.MoveDirection.x <= 0.0f)
+			.Where(x => _arrows[1].transform.localScale.x > 1.0f)
+			.Subscribe(_ => {
+				_arrows[1].transform.localScale -= Vector3.one * _scalingSpeed * Time.deltaTime;
+				if (_arrows[1].transform.localScale.x < 1.0f) {
+					_arrows[1].transform.localScale = Vector3.one;
+				}
+			});
+
+		// === 上の矢印 ===
+		// 拡大
+		this.ObserveEveryValueChanged(y => mm.MoveDirection.z)
+			.Where(z => z > 0.0f)
+			.Where(_ => _arrows[2].transform.localScale.y < _scaleLimit)
+			.Subscribe(_ => {
+				_arrows[2].transform.localScale += Vector3.one * _scalingSpeed * Time.deltaTime;
+				if (_arrows[2].transform.localScale.y > _scaleLimit) {
+					_arrows[2].transform.localScale = Vector3.one * _scaleLimit;
+				}
+			});
+		// 縮小
+		this.UpdateAsObservable()
+			.Where(_ => mm.MoveDirection.z <= 0.0f)
+			.Where(_ => _arrows[2].transform.localScale.y > 1.0f)
+			.Subscribe(_ => {
+				_arrows[2].transform.localScale -= Vector3.one * _scalingSpeed * Time.deltaTime;
+				if (_arrows[2].transform.localScale.y < 1.0f) {
+					_arrows[2].transform.localScale = Vector3.one;
+				}
+			});
+
+		// === 下の矢印 ===
+		// 拡大
+		this.ObserveEveryValueChanged(y => mm.MoveDirection.z)
+			.Where(z => z < 0.0f)
+			.Where(_ => _arrows[3].transform.localScale.y < _scaleLimit)
+			.Subscribe(_ => {
+				_arrows[3].transform.localScale += Vector3.one * _scalingSpeed * Time.deltaTime;
+				if (_arrows[3].transform.localScale.y > _scaleLimit) {
+					_arrows[3].transform.localScale = Vector3.one * _scaleLimit;
+				}
+			});
+		// 縮小
+		this.UpdateAsObservable()
+			.Where(_ => mm.MoveDirection.z >= 0.0f)
+			.Where(_ => _arrows[3].transform.localScale.y > 1.0f)
+			.Subscribe(_ => {
+				_arrows[3].transform.localScale -= Vector3.one * _scalingSpeed * Time.deltaTime;
+				if (_arrows[3].transform.localScale.y < 1.0f) {
+					_arrows[3].transform.localScale = Vector3.one;
+				}
+			});
 	}
 }
