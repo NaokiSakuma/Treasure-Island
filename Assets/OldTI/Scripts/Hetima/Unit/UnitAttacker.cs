48e5a8620c160717f524ab278233fa4de161373b
sakuma <soramamenatan@gmail.com>
2018-04-16 02:21:42 +0900

    敵ユニットが拠点に向かうように　拠点を攻撃するようにした

--- Assets/OldTI/Scripts/Hetima/Unit/UnitAttacker.cs
+++ Assets/OldTI/Scripts/Hetima/Unit/UnitAttacker.cs
@@ -9,12 +9,18 @@ public class UnitAttacker : MonoBehaviour {
 
 	UnitCore _core;
 
-	// Use this for initialization
-	void Start () {
+    // 拠点の位置
+    private Vector3 _basePos = Vector3.zero;
+
+    // Use this for initialization
+    void Start () {
 		_core = GetComponent<UnitCore>();
 
-		// 攻撃
-		this.UpdateAsObservable()
+        // inspecterでやった方がいいかもしれん　baseが動く可能性有なら修正しないと unitcoreに持たせるワンチャン
+        _basePos = GameObject.Find("base").transform.position;
+
+        // 攻撃
+        this.UpdateAsObservable()
 		    // 自分が生きていて
 		    .Where(_ => _core.Health > 0)
 		    // ターゲットがいる
@@ -27,9 +33,25 @@ public class UnitAttacker : MonoBehaviour {
 			.Subscribe(_ => {
 				Attack();
 			});
-	}
 
-	void Attack(){
+        this.UpdateAsObservable()
+            // 自分が生きていて
+            .Where(_ => _core.Health > 0)
+            // ターゲットがいない
+            .Where(_ => !_core.Target)
+            // ターゲットが攻撃の届く距離にいる
+            .Where(_ => Vector3.Distance(transform.position, _basePos) < _core.AttackReach)
+            // 一定間隔で
+            .ThrottleFirst(System.TimeSpan.FromSeconds(_core.AttackSpeed))
+            // 攻撃 拠点がわからんからとりあえずコンソールで表示
+            .Subscribe(_ => {
+                // 処理
+                print("attack");
+            });
+
+    }
+
+    void Attack(){
 
         //攻撃SE
         AudioManager.Instance.PlaySE(AUDIO.SE_BATTLE);
