67be43ba6965b5dd6f6c4e0fb0b2ee1a51d7dd5d
TSITCL\s162149 <yuta.programmer2016@gmail.com>
2018-05-02 17:34:04 +0900

    ステージ読み込み、nullリファレンスバグ修正

---
+++ Assets/Scripts/Gucchi/StageLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+namespace GucchiCS
+{
+    public class StageLoader : MonoBehaviour
+    {
+        // ステージリスト
+        [SerializeField]
+        List<GameObject> _stageList = new List<GameObject>();
+
+        // 選択したステージID
+        int _stageNo = -1;
+
+        void Awake()
+        {
+            this.ObserveEveryValueChanged(_ => _stageNo)
+                .Where(_ => _stageNo >= 0)
+                .Where(_ => _stageList.Count > 0)
+                .Subscribe(_ =>
+                {
+                    Instantiate(_stageList[_stageNo], Vector3.zero, Quaternion.identity);
+                });
+        }
+
+        // ステージ番号を設定
+        public int StageNo
+        {
+            get { return _stageNo; }
+            set { _stageNo = value; }
+        }
+    }
+}
