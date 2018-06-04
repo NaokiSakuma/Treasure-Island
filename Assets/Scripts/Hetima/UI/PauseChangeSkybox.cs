809187fc7f692d8331d04959ce14f28b5054257c
hetima333 <hetima333@gmail.com>
2018-06-04 12:22:25 +0900

    [fix] スカイボックスが見えないように修正

---
+++ Assets/Scripts/Hetima/UI/PauseChangeSkybox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseChangeSkybox : MonoBehaviour {
+
+	// 仮で色を設定しているのでインスペクタで調整したほうがいいかも
+	[SerializeField]
+	private Color _color = new Color(203,207,200,255);
+
+	void Start () {
+		var cam = Camera.main;
+		cam.clearFlags = CameraClearFlags.SolidColor;
+		cam.backgroundColor = _color;
+	}
+}
