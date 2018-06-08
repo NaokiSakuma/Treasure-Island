79d03c4de32863a9c139bb27e34f2a06e43fc59f
hetima333 <hetima333@gmail.com>
2018-06-08 17:54:20 +0900

    [update] ポーズメニューにゲームに戻るを追加
    他の項目についても同様に継承して作成可能なので項目が分かり次第作成

---
+++ Assets/Scripts/Hetima/UI/PauseBackToGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseBackToGame : SimplePauseItem {
+
+	[SerializeField]
+	private string _text = "ゲームに戻る";
+
+	void Start () {
+		GetComponentInChildren<TextMesh>().text = _text;
+	}
+
+	public override void OnClick(){
+		Pausable.Instance.pausing = false;
+	}
+}
