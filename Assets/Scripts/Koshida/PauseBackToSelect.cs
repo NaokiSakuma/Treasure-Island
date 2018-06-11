ebd1dc042419ab923d9f1c1b051b88b96f6e50be
konji <konjikk15@gmail.com>
2018-06-11 12:25:39 +0900

    ポーズ中に、ステージセレクトへ戻る、とリセット

---
+++ Assets/Scripts/Koshida/PauseBackToSelect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseBackToSelect : SimplePauseItem
+{
+
+    [SerializeField]
+    private string _text = "ステージセレクトへ";
+
+    void Start()
+    {
+        GetComponentInChildren<TextMesh>().text = _text;
+    }
+
+    public override void OnClick()
+    {
+        Pausable.Instance.pausing = false;
+        SceneManager.LoadScene("StageSelect");
+    }
+}
---
+++ Assets/Scripts/Koshida/PauseReset.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseReset : SimplePauseItem
+{
+
+    [SerializeField]
+    private string _text = "リセット";
+
+    void Start()
+    {
+        GetComponentInChildren<TextMesh>().text = _text;
+    }
+
+    public override void OnClick()
+    {
+        Pausable.Instance.pausing = false;
+
+        //ステージリセット
+        GetComponent<GucchiCS.ResetManager>().ResetObjects();
+    }
+}
