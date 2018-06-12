4cddccea51c071ac7ef85849cf03042ba6bdaa0b
gucchi <yuta.programmer2016@gmail.com>
2018-06-12 14:33:27 +0900

    ステージセレクトに戻った時に仮選択するステージを前回遊んでいたステージの扉に設定

---
+++ Assets/Scripts/Gucchi/StageNoReader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GucchiCS
+{
+    public class StageNoReader : SingletonMonoBehaviour<StageNoReader>
+    {
+        public static int _stageNo;
+
+        // Use this for initialization
+        void Start()
+        {
+            DontDestroyOnLoad(this);
+        }
+
+        // ステージ変更時
+        public int ChangeStageNo
+        {
+            set { _stageNo = value; }
+        }
+    }
+}
