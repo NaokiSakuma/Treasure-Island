cdf62dab1f5e7aa6f1fcd74c55b2a9c2e3f2b367
hetima333 <hetima333@gmail.com>
2018-06-16 19:00:02 +0900

    [add] タイトルの左にプレイヤーが移動した時にゲーム終了するかを問うように

---
+++ Assets/Scripts/Hetima/ButtonExitGame.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ButtonExitGame : MonoBehaviour {
+
+	private CanvasGroup _group;
+
+	[SerializeField, Range(0.0f, 2.0f)]
+	private float _fadeTime;
+
+	void Start () {
+		_group = GetComponent<CanvasGroup>();
+
+		// はじめは完全に透明にしておく
+		_group.alpha = 0.0f;
+		// フェードイン
+		_group.DOFade(1.0f, _fadeTime);
+
+		// 生成時にポーズ
+		Pausable.Instance.pausing = true;
+	}
+	
+	public void ExitGame(){
+		// ゲーム終了
+		Debug.Log("ここにゲーム終了処理");
+		// 念の為の記述、フェードアウト後ゲームに戻る
+		_group.DOFade(0.0f, _fadeTime).OnComplete(() => Destroy(this.gameObject));
+	}
+
+	public void BackToGame(){
+		// フェードアウト後ゲームに戻る
+		_group.DOFade(0.0f, _fadeTime)
+			.OnComplete(() => Destroy(this.gameObject));
+	}
+
+	void OnDestroy() {
+		Pausable.Instance.pausing = false;	
+	}
+}
